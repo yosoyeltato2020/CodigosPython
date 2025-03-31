f691066279b2a090b805c5e2def3efbbe2d39078
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-03-31 09:33:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from juego import jugar_maquina
+from juego import jugar_maquina, jugar_usuario
+
 
 def main():
-    print("\nBienvenido a " + "\033[93m" + "Piedra, Papel, Tijera, Lagarto, Spock" + "\033[0m")
-    print("¿Contra quién quieres jugar?")
-    print("1. Contra la máquina")
-
-    opcion = int(input("Introduce una opción (1): "))
+    print("Bienvenido al juego PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK")
+    print("¿Contra quién quieres jugar? 1: Máquina | 2: Otro usuario")
+    opcion = int(input("Introduce una opción 1 o 2: "))
     if opcion == 1:
         jugar_maquina()
+    elif opcion == 2:
+        jugar_usuario()
     else:
-        print("Opción no válida. Saliendo...")
+        print("Opción no válida. Inténtalo de nuevo.")
+    
+    jugar_de_nuevo = input("¿Quieres volver a jugar? (s/n): ").lower()
+    if jugar_de_nuevo == "s":
+        main
+
+
 main()
